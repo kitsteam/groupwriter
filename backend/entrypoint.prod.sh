07721ab6d5b8a5a2c67105e37dada1a4b0626c6b
Jannik Streek <jannik.streek@gmail.com>
2026-01-07 13:41:17 +0100

    revert migration rollback

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-echo "rollback failed migration"
-npx prisma migrate resolve --config dist/prisma.config.ts --rolled-back "20251123150223_add_owner_external_id"
-
 echo "Creating the schema..."
 npx prisma migrate deploy --config dist/prisma.config.ts
 
